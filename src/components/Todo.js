e5070278ccc543aab7eb3abcb31311df638a33d8
ITALO DORIA <italo.faria0@gmail.com>
2021-04-26 17:06:25 -0300

    newTodo-iframe

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -10,7 +10,7 @@ export default function Todo()
     return (
         <Fragment>
             <div className="todoContainer" onClick={closeTodo}>doit</div>
-            <iframe src="https://doit-eight.vercel.app/" className="todoContant todoClose"></iframe>
+            <iframe src="https://doit-eight.vercel.app/" title="doit" className="todoContant todoClose"></iframe>
         </Fragment>
     );
 }
